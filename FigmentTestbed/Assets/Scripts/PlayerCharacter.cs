7898dda1ec7c2850dc6472d6e954cd92756d4558
margo <michaelargo13@gmail.com>
2018-09-28 17:00:33 -0400

    added the base scripts for the playercharacter and the ghost

---
+++ FigmentTestbed/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    // Use this for initialization
+    public int lives;
+    public bool lightOn;
+    public bool lightBar;
+    public bool moving; 
+
+	void Start () {
+        lives = 3; 
+		
+	}
+	
+    void loseLife()//this will be called by the ghost actor
+    {
+        lives--;
+        if(lives <= 0)
+        {
+            //destroy actor
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+}
---
+++ FigmentTestbed/Assets/Scripts/ghost.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : MonoBehaviour {
+
+    public bool chasePlayer;
+    public bool isChasing;
+    public bool isAlive;
+    //startpoint of path
+    //endpoint of path
+
+
+	// Use this for initialization
+	void Start () {
+        isAlive = true;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this block defines the chasing ghost actions
+        if (chasePlayer)
+        {
+
+            //if chasePlayer && playerDist < x units, set is chasing to true
+
+            if (isChasing)
+            {
+                //go straight to player and avoid obstacles
+            }
+            
+
+            //if distance to player is > x units set isChasing to false
+
+        }
+        else
+        {
+            //follow predefined path
+        }
+
+        //if collides with player, call player.loselife and destroy actor
+        //if collides with light mesh destroy actor and play sound
+
+
+    }
+}
