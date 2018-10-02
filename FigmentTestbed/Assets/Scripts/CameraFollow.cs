4119fade455b811a31ec8f89c402391dc49bc39e
Your Name <you@example.com>
2018-10-02 18:15:18 -0400

    update

---
+++ FigmentTestbed/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	public float zOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, player.position.z - zOffset);
+	}
+}
